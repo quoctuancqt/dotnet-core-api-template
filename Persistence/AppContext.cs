187d0e2390e41794646724967878065ca8e044ec
quoctuan.cqqot <quoctuan.cqt@gmail.com>
2019-05-23 21:40:20 +0700

    Update Solution

--- Persistence/AppContext.cs
+++ Persistence/AppContext.cs
@@ -8,11 +8,20 @@ namespace Persistence
     public class AppContext : DbContext
     {
         private string UserId;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public AppContext(DbContextOptions<AppContext> options) : base(options)
         {
             UserId = Common.Factories.ResolverFactory.UserId;
         }
+
+        public CatalogDbContext(DbContextOptions<AppContext> options, IHttpContextAccessor httpContextAccessor)
+         : base(options)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public 
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
